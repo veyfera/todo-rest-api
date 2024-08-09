58b84dea4635b7f7b7d8d23ee1a636e6b6e8b84c
mozart <mozart@localhost.com>
2024-08-09 07:51:40 -0400

    modified validation logic, update todo now works

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -11,21 +11,26 @@ let todoCnt = 0;
 
 export const getTodos = (req: Request, res: Response) => {
     //const { filter, page = 1 } = req.params;
-    const { filter, page = 1 } = req.query;
-    if(filter) {
-        console.log("filtering todos by: ", filter);
-        //status
-        //priority
-        //ts
-        //
-        //
-        //sort by ts
+    const { filter, page = 1, status, priority, date } = req.query;
+    let searchRes:Array<Todo> = todos;
+    console.log("filtering todos by: ", filter);
+    switch(filter) {
+        case "status":
+            searchRes = todos.filter(t => t.status === +status);
+            break;
+        case "priority":
+            searchRes = todo.filter(t => t.priority === +priority)
+            break;
+        case "date":
+            searchRes = todo.filter(t => t.ts < date);
+            break;
     }
-    if(page > (TODO_PER_PAGE-1)*todos.length+1) {
+        //sort by ts
+    if(page > (TODO_PER_PAGE-1)*searchRes.length+1) {
         res.status(404).json({msg: "no such page"});
         return
     }
-    res.status(200).json(todos.slice(page-1, page*TODO_PER_PAGE));
+    res.status(200).json(searchRes.slice(page-1, page*TODO_PER_PAGE));
 }
 
 export const getTodo = (req: Request, res: Response) => {
@@ -43,34 +48,57 @@ export const createTodo = (req: Request, res: Response) => {
     if(title && description && status && priority) {
         let todo: Todo = {
             id: todoCnt.toString(),
-            title: title,//check_symbols
+            title: title,
             description: description,
             status: status,
             priority: priority,
             ts: Date.now()
         };
-        todo = validateTodo(todo);
-        todoCnt+=1;
-        todos.push(todo);
-        res.status(200).json({msg:"succesfully added new todo", id: todoCnt-1});
+        //todo = validateTodo(todo);
+        const { valid, msg } = validateTodo(todo);
+        if(valid) {
+            todoCnt+=1;
+            todos.push(todo);
+            res.status(200).json({msg:"succesfully added new todo", id: todoCnt-1});
+        } else {
+            res.status(400).json({msg: msg});
+        }
     } else {
         res.status(400).json({msg: "Invalid body, one or more properties missing: title, description, status, priority"});
     }
 }
 
 export const updateTodo = (req: Request, res: Response) => {
-    const { id, todo } = req.params;
-    if(!id || !todo) {
-        res.status(404);
+    const { id } = req.params;
+    const { title, description, status, priority} = req.body;
+
+    if(!id || !req.body) {
+        res.status(404).json({msg: "Please provide id and _ for updating a todo"});
         return;
     }
-    console.log(id, todo);
-    const oldTodo = todos.find(t => t.id === id);
-    if(oldTodo) {
-        if (oldTodo.status === Status.Complete || oldTodo.status === Status.Cancelled) {
-            oldTodo.status = todo.status;
-        } else {
-            oldTodo = {...oldTodo, ...todo}
+
+    let todo = todos.find(t => t.id === id);
+
+    if(todo) {
+        let tmpTodo: Todo = {
+            ...todo,
+            title: title,
+            description: description,
+            status: status,
+            priority: priority
+        }
+        const { valid, msg } =validateTodo(tmpTodo);
+
+        if(valid) {
+            if (todo.status === Status.Complete || todo.status === Status.Cancelled) {
+                todo.status = status || todo.status;
+                res.status(200).json({msg: "Succesfully updated todo with id " + id + " (can only update 'status' for completed and cancelled todos)"});
+                return;
+            }
+            todo.title = title || todo.title;
+            todo.description = description || todo.description;
+            todo.status = status || todo.status;
+            todo.priority = priority || todo.priority;
         }
         res.status(200).json({msg: "Succesfully updated todo with id " + id});
     } else {
@@ -91,9 +119,28 @@ export const deleteTodo = (req: Request, res: Response) => {
 }
 
 const validateTodo = (todo: Todo) => {
-    todo.title = todo.title.replace(/[^A-z 0-9.,?!-'"ЁёА-я]/g, "").slice(0, TODO_TITLE_LEN);
-    todo.description = todo.description.slice(0, TODO_DESC_LEN);
-    todo.status = Object.values(Status).includes(todo.status) ? todo.status : Status.Draft;
-    todo.priority = Object.values(Priority).includes(todo.priority) ? todo.priority : Priority.Low;
-    return todo;
+    //todo.title = todo.title.replace(/[^A-z 0-9.,?!-'"ЁёА-я]/g, "").slice(0, TODO_TITLE_LEN);
+    if (todo.title.length > TODO_TITLE_LEN) {
+        return {valid: false, msg: `Todo title should not be longer than ${TODO_TITLE_LEN}`};
+    }
+    if (todo.title.match(/[^A-z 0-9.,?!\-'"ЁёА-я]/g)) {
+        return {valid: false, msg: "Todo title should only contain latin, cyrilic, numeric and .,?!-\"' symbols"};
+    }
+
+    //todo.description = todo.description.slice(0, TODO_DESC_LEN);
+    if (todo.description.length > TODO_DESC_LEN) {
+        return {valid: false, msg: `Todo description should not be longer than ${TODO_DESC_LEN}`};
+    }
+
+    //todo.status = Object.values(Status).includes(todo.status) ? todo.status : Status.Draft;
+    if(!Object.values(Status).includes(todo.status)) {
+        return {valid: false, msg: "Todo status should be one of "};
+    }
+
+    //todo.priority = Object.values(Priority).includes(todo.priority) ? todo.priority : Priority.Low;
+    if(!Object.values(Priority).includes(todo.priority)) {
+        return {valid: false, msg: "Todo priority should be one of "};
+    }
+    //return todo;
+    return {valid: true};
 }
